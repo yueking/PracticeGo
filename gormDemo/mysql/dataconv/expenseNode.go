7a61d6edbb63a2262349d67baeb75c1aa9e97905
yueking <yuekinger@gmail.com>
2023-10-19 21:10:48 +0800

    gorm mysql model expenseNode update

--- gormDemo/mysql/dataconv/expenseNode.go
+++ gormDemo/mysql/dataconv/expenseNode.go
@@ -1,4 +1,25 @@
 package dataconv
 
+import "time"
+
+type BaseNode struct {
+	Owner       string
+	OwnerRegion string
+	Region      string
+	Stage       string
+	Status      string
+	Hid         string
+	Product     string
+	Agree       string
+	Payment     string
+}
 type ExpenseNode struct {
+	Base
+	Description    string
+	ExpenseNodeId  string
+	Money          float64
+	Number         int64
+	OccurrenceTime time.Time
+	Type           string
+	ExpenseId      string
 }
