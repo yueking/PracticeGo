bcbba1bf68b1bc1760c5e160e1d9e5dbd2c09d08
yueking <yuekinger@gmail.com>
2023-10-19 17:39:13 +0800

    gorm

--- gormDemo/app/main.go
+++ gormDemo/app/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"PracticeProjects/gormDemo/oracle/dataconv"
+	"PracticeProjects/gormDemo/mysql/dataconv"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	fmt.Println("starting..")
+	fmt.Println("starting")
+
 	dsn := "root:Mylife1586@tcp(127.0.0.1:3306)/db2?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
